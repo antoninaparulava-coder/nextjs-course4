803fbcc4e6efaa23af3251bf95a5669f8005f1ed
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-09-02 13:22:42 +0400

    Sending HTTP Requests To API Routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,22 +1,27 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
 
 async function handler(req, res) {
-    if(req.method === 'POST'){
-        const data = req.body;
+  if (req.method === 'POST') {
+    const data = req.body;
 
-        const client = await MongoClient.connect('mongodb+srv://antoninaparulava_db_user:2p8618Bef50E8Ow8@cluster0.zztfbxa.mongodb.net/meetups?retryWrites=true&w=majority');
-        const db = client.db();
+    const client = await MongoClient.connect(
+      'mongodb+srv://antoninaparulava_db_user:2p8618Bef50E8Ow8@cluster0.zztfbxa.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
 
-        const meetupsCollection = db.collection('meetups')
+    const meetupsCollection = db.collection('meetups');
 
-        const result = await meetupsCollection.insertOne(data);
+    const result = await meetupsCollection.insertOne(data);
 
-        console.log(result);
+    console.log(result);
 
-        client.close();
+    client.close();
 
-        res.status(201).json({message: 'Meetup inserted!'})
-    }
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
 }
 
 export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,26 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
-function NewMettupPage(){
-    function addMeetUpHandler(enteredMeetupData){
-        console.log(enteredMeetupData);
+function NewMeetupPage(){
+    const router = useRouter();
+
+    async function addMeetUpHandler(enteredMeetupData){
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        router.push('/')
     }
 
     return <NewMeetupForm onAddMeetup={addMeetUpHandler} />
 }
 
-export default NewMeetupForm
+export default NewMeetupPage
