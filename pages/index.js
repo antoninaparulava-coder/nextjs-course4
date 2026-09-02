7f1a6ebedfb6a91c816c4b76020d16f18b6b88c9
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-09-02 20:40:28 +0400

    Adding head Metadata To Pages

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,17 @@
 import MeetupList from "@/components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
-
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Mettup',
-        image: 'https://www.jasminealley.com/wp-content/uploads/2024/08/crown-anchor-london.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!'
-    },
-    {
-        id: 'm2',
-        title: 'A Second Mettup',
-        image: 'https://www.jasminealley.com/wp-content/uploads/2024/08/crown-anchor-london.jpg',
-        address: 'Some address 10, 12345 Some City',
-        description: 'This is a second meetup!'
-    }
-]
+import Head from "next/head";
+import { Fragment } from "react";
 
 function HomePage(props){
-
     return(
+        <Fragment>
+            <Head>
+                <title>React Meetups</title>
+                <meta name="description" content="Browse a huge list of a highly active React Meetups!" />
+            </Head>
             <MeetupList meetups={props.meetups} />
+        </Fragment>
     )
 }
 
